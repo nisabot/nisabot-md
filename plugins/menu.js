8a333f227634d39679d88f756ab1f000b76ac89f
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-13 09:11:42 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -160,8 +160,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     let url = `https://i.postimg.cc/nrMn3sMN/menu.jpg`
     conn.sendHydrated(m.chat, text.trim(), 'Void Great Emperor', null, 'https://youtube.com/channel/UCGAsDPrOq2irv9xqpG-NEuw', 'YOUTUBE MBY', '', '', [
       ['Donasi', '/donasi'],
-      ['?Sewa?', '/sewa'],
-      ['!Owner!', '/owner']
+      ['Sewa', '/sewa'],
+      ['Owner', '/owner']
     ], m)
     /*let url = `https://telegra.ph/file/ab1df70dfd5c2bac64da1.jpg`.trim()
     let res = await fetch(url)
