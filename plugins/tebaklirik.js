488e5499068aab96a4a74e708af4cbe901d22df5
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-14 08:38:19 +0700

    Update tebaklirik.js

--- plugins/tebaklirik.js
+++ plugins/tebaklirik.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
 
-let timeout = 180000
+let timeout = 120000
 let poin = 500
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik : {}
