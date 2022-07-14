13aa2177f3f373e49f13ab48d9df46abd59606f3
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-14 08:39:29 +0700

    Update tebaklagu.js

--- plugins/tebaklagu.js
+++ plugins/tebaklagu.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 
-let timeout = 180000
+let timeout = 120000
 let poin = 1000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebaklagu = conn.tebaklagu ? conn.tebaklagu : {}
