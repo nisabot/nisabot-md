d96149b8a8e84661cda8c80bd10ac97c79b75084
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-19 14:05:22 +0700

    Update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 50000
+let poin = 5000
 let handler = async (m, { conn, usedPrefix }) => {
   conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
   let id = m.chat
