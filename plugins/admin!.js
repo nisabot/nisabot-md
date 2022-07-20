01859612e0e448f7c462c5b041df33e00b408572
nisabot <109346129+nisabot@users.noreply.github.com>
2022-07-20 20:38:15 +0700

    Update admin!.js

--- plugins/admin!.js
+++ plugins/admin!.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Jadi kan bot sebagai admin'
-  if (isAdmin) throw 'Padahal udah jadi admin'
+  if (m.fromMe) throw 'Jadiken hela botna jadi admin goblok'
+  if (isAdmin) throw 'Padahal ges jadi admin manehteh'
   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
 }
 handler.command = /^admin!$/i
