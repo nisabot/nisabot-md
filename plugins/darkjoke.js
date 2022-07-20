3c6aa6bae6e689f80c1c4a052ff7e05463fe6792
nisabot <109346129+nisabot@users.noreply.github.com>
2022-07-20 20:08:45 +0700

    Update darkjoke.js

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let rest = 'https://api.zacros.my.id/randomimg/darkjokes'
-    conn.sendButtonImg(m.chat, rest, 'Dark? 🤨', wm2, 'NEXT', '.darkjokes', m)
+    conn.sendButtonImg(m.chat, rest, 'Dark? 🤨', wm, 'NEXT', '.darkjokes', m)
 }
 handler.help = ['darkjokes']
 handler.tags = ['internet']
